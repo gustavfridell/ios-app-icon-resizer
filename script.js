5a2d36658004f72ad27340ec1cc157ae9c05e897
Gustav Fridell <gustavfridell@me.com>
2016-12-27 23:32:47 +0100

    make it possible to resize image

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var canvas = document.createElement('canvas')
-var context = canvas.getContext('2d')
 var imageUploadZone = document.querySelector('#imageUploadZone')
 
 function handleFileUpload (e) {
@@ -10,7 +8,7 @@ function handleFileUpload (e) {
     var files = e.target.files || e.dataTransfer.files
     var file = files && files[0]
 
-    if (!(file && file.type.match('image.*'))) {
+    if (!(file && file.type.match('image.png'))) {
         return false
     }
 
@@ -18,7 +16,27 @@ function handleFileUpload (e) {
     reader.onload = function (e) {
         var img = new Image()
         img.onload = function () {
+            // save image in original size
+            var canvas = document.createElement('canvas')
+            var context = canvas.getContext('2d')
+            canvas.width = img.width
+            canvas.height = img.height
             context.drawImage(img, 0, 0)
+
+
+            // MOVE THIS
+            var size = 100
+            var resizedCanvas = document.createElement('canvas')
+            var resizedContext = resizedCanvas.getContext('2d')
+            resizedCanvas.width = size
+            resizedCanvas.height = size
+            resizedContext.drawImage(canvas, 0, 0, size, size)
+
+            var data = resizedCanvas.toDataURL('image/png')
+            var link = document.createElement('a')
+            link.setAttribute('href', data)
+            link.setAttribute('download', 'resized-image.png')
+            link.click()
         }
         img.src = e.target.result
     }
@@ -40,7 +58,7 @@ function handleDragleave () {
 }
 
 imageUploadZone.addEventListener('change', handleFileUpload, false)
-imageUploadZone.addEventListener('dragover', handleDragover, false)
 imageUploadZone.addEventListener('dragenter', handleDragenter, false)
+imageUploadZone.addEventListener('dragover', handleDragover, false)
 imageUploadZone.addEventListener('dragleave', handleDragleave, false)
 imageUploadZone.addEventListener('drop', handleFileUpload, false)
