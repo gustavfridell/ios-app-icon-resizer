2fa3a8350bf83d1a6ece27dad60cbf5329bac6de
Gustav Fridell <gustavfridell@me.com>
2016-12-28 13:17:07 +0100

    add pica library for resizing

--- script.js
+++ script.js
@@ -1,4 +1,78 @@
 var imageUploadZone = document.querySelector('#imageUploadZone')
+// https://developer.apple.com/library/content/qa/qa1686/_index.html
+var imageSizes = [
+    {
+        name: 'iTunesArtwork',
+        size: 512
+    },
+    {
+        name: 'iTunesArtwork@2x',
+        size: 1024
+    },
+    {
+        name: 'Icon-60@2x.png',
+        size: 120
+    },
+    {
+        name: 'Icon-60@3x.png',
+        size: 180
+    },
+    {
+        name: 'Icon-76.png',
+        size: 76
+    },
+    {
+        name: 'Icon-76@2x.png',
+        size: 152
+    },
+    {
+       name: 'Icon-83.5@2x.png',
+       size: 167
+    },
+    {
+        name: 'Icon-Small-40.png',
+        size: 40
+    },
+    {
+        name: 'Icon-Small-40@2x.png',
+        size: 80
+    },
+    {
+        name: 'Icon-Small-40@3x.png',
+        size: 120
+    },
+    {
+        name: 'Icon-Small.png',
+        size: 29
+    },
+    {
+        name: 'Icon-Small@2x.png',
+        size: 58
+    },
+    {
+        name: 'Icon-Small@3x.png',
+        size: 87
+    }
+]
+
+function resizeImage (originalCanvas) {
+    var size = 100
+    var resizedCanvas = document.createElement('canvas')
+    resizedCanvas.width = size
+    resizedCanvas.height = size
+
+    pica.resizeCanvas(originalCanvas, resizedCanvas, {}, function (error) {
+        if (error) {
+            return console.error(error)
+        }
+
+        var data = resizedCanvas.toDataURL('image/png')
+        var link = document.createElement('a')
+        link.setAttribute('href', data)
+        link.setAttribute('download', 'resized-image.png')
+        link.click()
+    })
+}
 
 function handleFileUpload (e) {
     e.stopPropagation()
@@ -16,27 +90,12 @@ function handleFileUpload (e) {
     reader.onload = function (e) {
         var img = new Image()
         img.onload = function () {
-            // save image in original size
             var canvas = document.createElement('canvas')
             var context = canvas.getContext('2d')
             canvas.width = img.width
             canvas.height = img.height
             context.drawImage(img, 0, 0)
-
-
-            // MOVE THIS
-            var size = 100
-            var resizedCanvas = document.createElement('canvas')
-            var resizedContext = resizedCanvas.getContext('2d')
-            resizedCanvas.width = size
-            resizedCanvas.height = size
-            resizedContext.drawImage(canvas, 0, 0, size, size)
-
-            var data = resizedCanvas.toDataURL('image/png')
-            var link = document.createElement('a')
-            link.setAttribute('href', data)
-            link.setAttribute('download', 'resized-image.png')
-            link.click()
+            resizeImage(canvas)
         }
         img.src = e.target.result
     }
